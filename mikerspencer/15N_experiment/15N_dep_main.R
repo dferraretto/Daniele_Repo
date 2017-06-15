25f5ee3429bcd85640e644da64eef819ad5d0df5
dferraretto <s1373890@ed.ac.uk>
2017-06-15 20:03:57 +0100

    corrected the .csv to take into account of the blanks for NH4 and NO3. Completed plot with horizontal lines showing the applied N vs. cumulative N collected under the canopy.

--- mikerspencer/15N_experiment/15N_dep_main.R
+++ mikerspencer/15N_experiment/15N_dep_main.R
@@ -17,6 +17,8 @@ setwd("C:/Users/s1373890/Daniele_Repo")
 library(readr)
 N_Ndep_15N_simplified <- read_csv("C:/Users/s1373890/Daniele_Repo/15N_experiment/15N_fieldlab/15N_Ndep_simplified.csv")
 
+#### NOTE: all NH4 and NO3 have been cleaned by blanks in the .csv file  ########
+
 # Trees canopy surface (m2) as from Excel calculations - "STAR" model simplified
 T1.area = 11.8
 T2.area = 11.5
@@ -57,7 +59,7 @@ TFSF.15N = N_Ndep_15N_simplified[which(N_Ndep_15N_simplified$Sample == "T1" | N_
 # | N_Ndep_15N_simplified$Sample == "T13T1" | N_Ndep_15N_simplified$Sample == "T13T2" | N_Ndep_15N_simplified$Sample == "T13T3"
 
 # drop all but masses
-TFSF.15N = TFSF.15N[ , c(1,2,16,17)]
+TFSF.15N = TFSF.15N[ , c(1,2,18,19)]
 
 #substring Samples to the number of trees
 TFSF.15N$tree = substr(TFSF.15N$Sample, start=2, stop=2)
@@ -74,13 +76,27 @@ SF15 = TFSF.15N.long[which(TFSF.15N.long$Sample== "S1" | TFSF.15N.long$Sample== 
 TF15 = TF15[with(TF15, order(Date, Sample, tree)), ]
 SF15 = SF15[with(SF15, order(Date, Sample, tree)), ]
 
+# merge TF and SF
 TFSF.Nx = merge(TF15, SF15, by = c("Date", "variable", "tree"), all = FALSE)
-TFSF.Nx$Nmass = TFSF.Nx$value.x + TFSF.Nx$value.y
 
+# Scaling the masses to each tree canopy (https://stackoverflow.com/questions/29709248/multiplying-column-value-by-another-value-depending-on-value-in-certain-column-r)
+TFSF.Nx = transform(TFSF.Nx, newvalue.x=ifelse(tree==1, value.x*T1.area / T1.TF.coll, 
+                              ifelse(tree==2, value.x*T2.area / T2.TF.coll,
+                              ifelse(tree==3, value.x*T3.area / T3.TF.coll, NA))))
+TFSF.Nx = transform(TFSF.Nx, newvalue.y=ifelse(tree==1, value.y*T1.area / T1.TF.coll, 
+                              ifelse(tree==2, value.y*T2.area / T2.TF.coll,
+                              ifelse(tree==3, value.y*T3.area / T3.TF.coll, NA))))
+
+
+TFSF.Nx = TFSF.Nx[ , c(1:3,8,9)]
+names(TFSF.Nx) = c("date","N_form", "tree", "TF_N","SF_N")
+
+# overall N (TF+SF) by form
+TFSF.Nx$Nmass = TFSF.Nx$TF_N + TFSF.Nx$SF_N
 
 # Define season
 TFSF.Nx$season <- "summer"
-TFSF.Nx$season[TFSF.Nx$Date > "2016-12-31"] <- "winter"
+TFSF.Nx$season[TFSF.Nx$date > "2016-12-31"] <- "winter"
 
 summer.Nx = TFSF.Nx[TFSF.Nx$season == "summer",]
 winter.Nx = TFSF.Nx[TFSF.Nx$season == "winter",]
@@ -90,21 +106,72 @@ winter.Nx = TFSF.Nx[TFSF.Nx$season == "winter",]
 
 require(data.table)
 summer.Nx <- data.table(summer.Nx)
-summer.Nx = summer.Nx[, Cum.Sum := cumsum(Nmass),by=list(variable, tree)]
+summer.Nx = summer.Nx[, Cum.Sum := cumsum(Nmass),by=list(N_form, tree)]
 winter.Nx <- data.table(winter.Nx)
-winter.Nx = winter.Nx[, Cum.Sum := cumsum(Nmass), by=list(variable, tree)]
+winter.Nx = winter.Nx[, Cum.Sum := cumsum(Nmass), by=list(N_form, tree)]
 
-
+rm(SF15, TF15, TF.2.remove, TFSF.15N.long, TFSF.15N, TFSF.Nx)
 
 ##########################     PLOT N mass
 library(ggplot2)
 
-# cumulative N per application
-ggplot(data = summer.Nx, aes (Date, Cum.Sum)) + geom_smooth() + facet_grid(variable ~ .)
-ggplot(data = winter.Nx, aes (Date, Cum.Sum)) + geom_smooth() + facet_grid(variable ~ .)
+#extract the application N data
 
-# cosa manca: al di la delle estetiche, i dati di massa non sono ancora "scalati", vanno moltiplicati per il 
-# fattore Tx.area/x.TF.coll. Poi va creato un valore cumulativo da plottare per compararlo col valore spruzzato (line to be added)
+N_app = N_Ndep_15N_simplified[grep("T13T", N_Ndep_15N_simplified$Sample),c(1,2,4,5)]
+N_app$NH4.N = N_app$NH4_lab*14/18
+N_app$NO3.N = N_app$NO3_lab*14/62
+N_app =N_app[ , c(1,2,5,6)]
+
+library(reshape2)
+N_app_long <- melt(N_app, id.vars = c("Date", "Sample"))
+N_app_sum = N_app_long[N_app_long$Date == "2016-08-05", ]
+
+
+# cumulative N per application
+dummy1 <- data.frame(X = c("NH4.N", "NO3.N"), Z = c(1335.444, 1332.710))
+dummy2 <- data.frame(X = c("NH4.N", "NO3.N"), Z = c(1494.889, 1491.903))
+
+
+ggplot(data = summer.Nx, aes (date, Cum.Sum)) + geom_smooth(color = 'red4') + facet_grid(N_form ~ .) +
+    ggtitle("Cumulative N mass collected in throughfall \nand stemflow after the winter application") +
+    labs( x = "date", y = "cumulative N mass (mg/tree)") + theme_bw(base_size = 12) +
+    geom_hline(data = dummy1, aes(yintercept = Z), color = "red3") +
+      theme(plot.title = element_text(hjust = 0.1, size = 18, colour = 'red4'),
+          plot.background = element_rect(fill = "transparent",colour = NA)) 
+
+
+ggplot(data = winter.Nx, aes (date, Cum.Sum)) + geom_smooth(color = 'red4') + facet_grid(N_form ~ .) +
+    ggtitle("Cumulative N mass collected in throughfall \nand stemflow after the winter application") +
+  labs( x = "date", y = "cumulative N mass (mg/tree)") + theme_bw(base_size = 12) +
+  geom_hline(data = dummy2, aes(yintercept = Z), color = "blue", linetype = 3, size = 1) +
+  theme(plot.title = element_text(hjust = 0.1, size = 18, colour = 'red4'),
+        plot.background = element_rect(fill = "transparent",colour = NA)) 
+
+
+
+# OK ho messo una linea che poi e' il valore spruzzato su T1. Come ci metto anche T2 e T3 come se fossero alto e basso?
+# OPPURE: Creo un cumulativo di spraying iniziale piu' valore di RF! mmmm, complicato pero' ne ho pochissimi e 
+# non ho la profondita' quindi il vol/m.
+# Ne esco meglio mostrando dove control e treatment si equivalgono e quindi ci metto una linea verticale
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
